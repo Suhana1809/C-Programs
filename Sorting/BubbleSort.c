0c9f74c6ce7b219ce838c584b61be536056ac770
Suhana1809 <guriasuhana@gmail.com>
2026-09-23 14:20:50 +0530

    Added Bubble Sort implementation
    Implement Bubble Sort algorithm and demonstrate sorting in main function.

---
+++ Sorting/BubbleSort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void bubble_sort(int array[],int size)
+{
+    for(int step=0; step<size-1; step++)
+    {
+        for(int i=0; i<size-step-1; i++)
+        {
+            if(array[i]>array[i+1])
+            {
+                int temp=array[i];
+                array[i]=array[i+1];
+                array[i+1]=temp;
+            }
+        }
+    }
+}
+int main()
+{
+    int data[] = {6,4,7,8,1,9,5};
+    int size = sizeof(data)/sizeof(int);
+
+    bubble_sort(data,size);
+
+    printf("Sorted array:\n");
+
+    for(int i=0; i<size; i++)
+    {
+        printf("%d",data[i]);
+    }
+}
